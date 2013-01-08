fc861dfcd45176bcf0046cf4e57613d416637de3
Roland Michael dela Pena <roland@kapitbisig.com>
2013-01-08 23:12:47 +0800

    Issue by arpeggio: Removed duplicate javascript variable settings 'ask_geolocation'.

--- modules/device_geolocation/js/device_geolocation_check.js
+++ modules/device_geolocation/js/device_geolocation_check.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.deviceGeolocationCheck = {
     attach: function (context, settings) {
       $.ajax({
-        url:  Drupal.settings.basePath + 'check-geolocation-attempt',
+        url:  settings.basePath + 'check-geolocation-attempt',
         type: 'POST',
         dataType: 'json',
         success: function(data) {
