afa437d0683a940db06e28657cffc296d920540b
arpeggio <arpeggio@467092.no-reply.drupal.org>
2016-08-14 11:33:45 +0800

    Issue #2783663 by arpeggio: Updated IP2Location library.

--- includes/vendor/composer/autoload_classmap.php
+++ includes/vendor/composer/autoload_classmap.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'IP2Location\\Database' => $vendorDir . '/ip2location/ip2location-php/IP2Location.php',
 );
